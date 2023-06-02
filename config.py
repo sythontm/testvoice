36155fd128c7b004a7355cc1a93a2b925f23e006
SYTHON <124038843+sythontm@users.noreply.github.com>
2023-06-02 14:04:17 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,88 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+import os
+APP_ID = os.environ.get("APP_ID")
+APP_HASH = os.environ.get("APP_HASH")
+
+CHNA = os.environ.get("CHNA")
+
+APP_IDD = os.environ.get("APP_IDD")
+APP_HASHH = os.environ.get("APP_HASHH")
+
+APP_IDDD = os.environ.get("APP_IDDD")
+APP_HASHHH = os.environ.get("APP_HASHHH")
+
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
+session1 = os.environ.get("TERMUX")
+SESSION1 = os.environ.get("TERMUX")
+token = os.environ.get("TOKEN")
+DEVLOO = os.environ.get("DEVLO")
+
+sython1 = TelegramClient(StringSession(session1), APP_ID, APP_HASH)
+bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
+
+
+session2 = os.environ.get("TERMUXX")
+SESSION2 = os.environ.get("TERMUXX")
+sython2 = TelegramClient(StringSession(session2), APP_ID, APP_HASH)
+
+
+session3 = os.environ.get("TERMUXXX")
+SESSION3 = os.environ.get("TERMUXXX")
+sython3 = TelegramClient(StringSession(session3), APP_ID, APP_HASH)
+
+
+session4 = os.environ.get("TERMUXXXX")
+SESSION4 = os.environ.get("TERMUXXXX")
+sython4 = TelegramClient(StringSession(session4), APP_ID, APP_HASH)
+
+
+session5 = os.environ.get("TERMUXXXXX")
+SESSION5 = os.environ.get("TERMUXXXXX")
+sython5 = TelegramClient(StringSession(session5), APP_ID, APP_HASH)
+
+session6 = os.environ.get("TERMUXSI")
+SESSION6 = os.environ.get("TERMUXSI")
+sython6 = TelegramClient(StringSession(session6), APP_IDD, APP_HASHH)
+
+
+session7 = os.environ.get("TERMUXSE")
+SESSION7 = os.environ.get("TERMUXSE")
+sython7 = TelegramClient(StringSession(session7), APP_IDD, APP_HASHH)
+
+session8 = os.environ.get("TERMUXEI")
+SESSION8 = os.environ.get("TERMUXEI")
+sython8 = TelegramClient(StringSession(session8), APP_IDD, APP_HASHH)
+
+session9 = os.environ.get("TERMUXNI")
+SESSION9 = os.environ.get("TERMUXNI")
+sython9 = TelegramClient(StringSession(session9), APP_IDD, APP_HASHH)
+
+session10 = os.environ.get("TERMUXTE")
+SESSION10 = os.environ.get("TERMUXTE")
+sython10 = TelegramClient(StringSession(session10), APP_IDD, APP_HASHH)
+
+session11 = os.environ.get("TERMUXEL")
+SESSION11 = os.environ.get("TERMUXEL")
+sython11 = TelegramClient(StringSession(session11), APP_IDDD, APP_HASHHH)
+
+session12 = os.environ.get("TERMUXTW")
+SESSION12 = os.environ.get("TERMUXTW")
+sython12 = TelegramClient(StringSession(session12), APP_IDDD, APP_HASHHH)
+
+session13 = os.environ.get("TERMUXTH")
+SESSION13 = os.environ.get("TERMUXTH")
+sython13 = TelegramClient(StringSession(session13), APP_IDDD, APP_HASHHH)
+
+session14 = os.environ.get("TERMUXFO")
+SESSION14 = os.environ.get("TERMUXFO")
+sython14 = TelegramClient(StringSession(session14), APP_IDDD, APP_HASHHH)
+
+session15 = os.environ.get("TERMUXFI")
+SESSION15 = os.environ.get("TERMUXFI")
+sython15 = TelegramClient(StringSession(session15), APP_IDDD, APP_HASHHH)
+
+ispay = ['yes']
+ispay2 = ['yes']
+bot.start()
+
